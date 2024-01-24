c3de982165ea8ea85b2e3e6f75d329829bfa0714
ayush kumar <ayushkumar@ayushs-MacBook-Pro.local>
2024-01-24 17:19:07 +0530

    fixed date date due to zone diff in build

--- src/utils/dateUtils.test.ts
+++ src/utils/dateUtils.test.ts
@@ -1,9 +1,21 @@
-import { formatDateTime } from "./dateUtils";
+import { formatDate, formatDateTime, formatTime } from "./dateUtils";
 
 describe("should format the date and time", () => {
   test("should format dateTime", () => {
-    const date = "2024-01-21T08:47:08.470Z";
-    const expectedDateTime = "Jan 21, 2024 2:17 PM";
+    const date = new Date("2024-01-21T08:47:08.470Z").toISOString();
+    const expectedDateTime = "Jan 21, 2024 8:47 AM";
     expect(formatDateTime(date)).toEqual(expectedDateTime);
+  });
+
+  test("should format date", () => {
+    const date = new Date("2024-01-21T08:47:08.470Z").toISOString();
+    const expectedDateTime = "Jan 21, 2024";
+    expect(formatDate(date)).toEqual(expectedDateTime);
+  });
+
+  test("should format time", () => {
+    const date = new Date("2024-01-21T08:47:08.470Z").toISOString();
+    const expectedDateTime = "8:47 AM";
+    expect(formatTime(date)).toEqual(expectedDateTime);
   });
 });
